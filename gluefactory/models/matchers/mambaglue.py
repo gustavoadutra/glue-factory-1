ef5ac9f35cea4342021946915dce3e1258456849
aki <aki@email.com>
2025-08-24 11:53:51 -0300

    debug

--- gluefactory/models/matchers/mambaglue.py
+++ gluefactory/models/matchers/mambaglue.py
@@ -626,7 +626,7 @@ class MambaGlue(BaseModel):
 
             # Load the state dict into your model
             self.load_state_dict(state_dict, strict=False)
-            print("Modelo baixado:", fname)
+            print("Modelo baixado:", state_dict)
         elif conf.weights is not None:
             path = Path(__file__).parent
             path = path / "weights/{}.pth".format(self.conf.weights)
