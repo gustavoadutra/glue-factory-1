4427a09115e58b6795a27dbfb6c4f193689c696e
aki <aki@email.com>
2025-08-24 18:14:01 -0300

    debug

--- gluefactory/models/matchers/mambaglue.py
+++ gluefactory/models/matchers/mambaglue.py
@@ -704,7 +704,7 @@ class MambaGlue(BaseModel):
     def _forward(self, data: dict) -> dict:
         for key in self.required_data_keys:
             assert key in data, f"Missing key {key} in data"
-        data0, data1 = data["image0"], data["image1"]
+        data0, data1 = data["view0"], data["view1"]
         kpts0, kpts1 = data0["keypoints"], data1["keypoints"]
         b, m, _ = kpts0.shape
         b, n, _ = kpts1.shape
