ca28dead94c201ea160e0ec65b2c20da055c624e
aki <aki@email.com>
2025-08-24 19:55:01 -0300

    debug

--- gluefactory/models/matchers/mambaglue.py
+++ gluefactory/models/matchers/mambaglue.py
@@ -711,9 +711,7 @@ class MambaGlue(BaseModel):
         print("==Keypoints shape==")
         print(kpts0.shape, kpts1.shape)
         device = kpts0.device
-        #size0, size1 = data0.get("image_size"), data1.get("image_size")
-        size0 = 640
-        size1 = 480 
+        size0, size1 = data0.get("image_size"), data1.get("image_size")
         print(size0, size1)
         kpts0 = normalize_keypoints(kpts0, size0).clone()
         kpts1 = normalize_keypoints(kpts1, size1).clone()
